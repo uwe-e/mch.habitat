20b319d0f9820fd6c48fa8b7e6f46bc908a53e34
uwe-e <uwe-e@users.noreply.github.com>
2017-07-17 11:57:32 +0200

    initial commit

---
+++ gulp-config.js
@@ -0,0 +1,15 @@
+module.exports = function () {
+    var instanceRoot = "D:\\Work\\Sitecore\\mchcms.dev.local";
+    var config = {
+        websiteRoot: instanceRoot + "\\Website",
+        sitecoreLibraries: instanceRoot + "\\Website\\bin",
+        licensePath: instanceRoot + "\\Data\\license.xml",
+        solutionName: "mch.habitat",
+        buildConfiguration: "Debug",
+        buildPlatform: "Any CPU",
+        buildToolsVersion: 15.0, //change to 14.0 for VS2015 support
+        publishPlatform: "AnyCpu",
+        runCleanBuilds: false
+    };
+    return config;
+}
